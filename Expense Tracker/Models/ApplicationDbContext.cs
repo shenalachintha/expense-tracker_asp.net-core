c8120de8e7f1c0e17ecfd70ecebef6f92d96e7b6
shenalachintha <156101606+shenalachintha@users.noreply.github.com>
2025-10-01 11:12:20 +0530

    commit change

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
